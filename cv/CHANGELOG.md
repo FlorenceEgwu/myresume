d2e7f6d9160780028db992374b547817f046ff7a
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-08-19 03:06:19 +0100

    Fix form submission issue and configure for deploy

---
+++ cv/CHANGELOG.md
@@ -0,0 +1,8 @@
+# MYRESUME CHANGELOG
+-  Moved static to app level
+-  Changed ContactForm to instance of ModelForm
+-  Add messages framework
+-  Change from Virtual env to pipenv and discard requirements.txt file
+-  Create name for cv app
+-  Add whitenoise for static files
+-  Setup heroku configs
--- cv/forms.py
+++ cv/forms.py
@@ -1,9 +1,7 @@
 from django import forms
+from . import models
 
-
-
-class ContactForm(forms.Form):
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    email_address = forms.EmailField(max_length=150)
-    message = forms.CharField(widget=forms.Textarea, max_length=2000)
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = models.Contact
+        fields = '__all__'
